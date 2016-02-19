9c088f077b02785ea914ba334d8364b5171fc615
AkitoshiAsakura <akitoshi.a@icloud.com>
2016-02-19 22:46:54 +0900

    Initial Commit

---
+++ Calendar/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Calendar
+//
+//  Created by 朝倉明俊 on H28/02/19.
+//  Copyright © 平成28年 朝倉明俊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
